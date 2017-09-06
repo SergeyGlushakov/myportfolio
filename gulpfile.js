4aec720984b586b2fea1e36ae279703321c86090
Dima Kovalchuk <kovaldn@gmail.com>
2017-09-06 15:12:49 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ const gulpWebpack = require('gulp-webpack');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js')
 
+// для удобства все пути в одном месте
 const paths = {
     root: './build',
     styles: {
@@ -26,15 +27,21 @@ const paths = {
     templates: {
         src: 'src/templates/**/*.pug',
         dest: 'build/assets/'
+    },
+    images: {
+        src: 'src/images/**/*.*',
+        dest: 'build/assets/images/'
     }
 };
 
+// pug
 function templates() {
     return gulp.src('./src/templates/pages/*.pug')
         .pipe(pug({ pretty: true }))
         .pipe(gulp.dest(paths.root));
 }
 
+// scss
 function styles() {
     return gulp.src('./src/styles/app.scss')
         .pipe(sourcemaps.init())
@@ -44,22 +51,33 @@ function styles() {
         .pipe(gulp.dest(paths.styles.dest))       
 }
 
+// webpack
 function scripts() {
     return gulp.src('src/scripts/app.js')
         .pipe(gulpWebpack(webpackConfig, webpack))
         .pipe(gulp.dest(paths.scripts.dest));
 }
 
+// очистка папки build
 function clean() {
-    return del('assets');
+    return del(paths.root);
 }
 
+// просто переносим картинки
+function images() {
+    return gulp.src(paths.images.src)
+          .pipe(gulp.dest(paths.images.dest));
+}
+
+// следим за src и запускаем нужные таски (компиляция и пр.)
 function watch() {
     gulp.watch(paths.scripts.src, scripts);
     gulp.watch(paths.styles.src, styles);
     gulp.watch(paths.templates.src, templates);
+    gulp.watch(paths.images.src, images);
 }
 
+// следим за build и релоадим браузер
 function server() {
     browserSync.init({
         server: paths.root   
@@ -67,11 +85,7 @@ function server() {
     browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
 }
 
-function images() {
-    return gulp.src('./src/images/**/*.*')
-          .pipe(gulp.dest(paths.root + '/assets/img'));
-}
-
+// экспортируем функции для доступа из терминала (gulp clean)
 exports.clean = clean;
 exports.styles = styles;
 exports.scripts = scripts;
@@ -80,6 +94,7 @@ exports.images = images;
 exports.watch = watch;
 exports.server = server;
 
+// сборка и слежка
 gulp.task('default', gulp.series(
     clean,
     gulp.parallel(styles, scripts, templates, images),
